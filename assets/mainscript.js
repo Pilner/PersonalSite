d9a0324a22a98cb77415febaa2aa3a6fb8a9a06b
Railey Victuelles <raileyvictuelles@gmail.com>
2022-10-21 20:25:48 +0800

    Revamped Website

--- assets/mainscript.js
+++ assets/mainscript.js
@@ -1,19 +1,7 @@
-var popoverTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="popover"]'));
-var popoverList = popoverTriggerList.map(function (popoverTriggerEl) {
-  return new bootstrap.Popover(popoverTriggerEl)
-});
-
-
-var mdmButton = document.querySelector("#mdm-website");
-
-// mdmButton.addEventListener("click", () => {
-// 	alert("There is no website yet, but you can go and check at my github!");
-// })
-
-let button = document.querySelectorAll(".alt-contacts-grid button").forEach(button => {
+let button = document.querySelectorAll(".contact .container .contact-dark .wrapper div .grid-container .grid-item button").forEach(button => {
 	button.addEventListener("click", () => {
 		navigator.clipboard.writeText(button.name);
-		button.querySelectorAll(".alt-contacts-grid button span").forEach(span => {
+		button.querySelectorAll(".contact .container .contact-dark .wrapper div .grid-container .grid-item button span").forEach(span => {
 			span.classList.toggle("visible");
 			span.classList.toggle("invisible");
 			setTimeout((event) => {
@@ -23,3 +11,5 @@ let button = document.querySelectorAll(".alt-contacts-grid button").forEach(butt
 		});
 	});
 });
+
+
